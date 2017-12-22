14630e226acdf0b3c989d409d608bf54c814414c
Yang <yang@cratebind.com>
2017-12-21 22:26:45 -0600

    add select filter buttons to filter listings by city, set up routes and action for location_search

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,6 +6,8 @@ class ListingsController < ApplicationController
 
 
 	def location_search
+		city = params[:city]
+		@listings = @resources.select{|r| r.address.include?(city)}
 	end
 
 	# Because I don't have a database,
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: "listings#index"
+  get 'listings/location_search/:city', to: 'listings#location_search', as: :location_search
 end
